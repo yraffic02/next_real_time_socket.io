2e6a1be58c00fa014b84d8a38705a38b3827c9af
e-pedidos <epedidosapp2023@gmail.com>
2023-12-19 16:02:26 -0300

    fix: corrigido header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,5 +1,7 @@
-export const Header = () =>{
+export const Header = () => {
+  return (
     <div className="w-screen flex items-center justify-center p-6">
-        <h1>Header</h1>
+      <h1>Header</h1>
     </div>
-}
+  );
+};
